7e15f62d54bae51e48d51d5a4222be45d64f50d0
Pathum Egodawatta <pathumego@gmail.com>
2015-07-01 17:47:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git init
 git config user.name "Pathum Egodawatta"
 git config user.email "pathum@mooniak.com"
 
-git remote add upstream "https://65a26bccf79e8c85c40a9583df5387265d642acf@github.com/mooniak/stick-no-billsfont.git"
+git remote add upstream "https://65a26bccf79e8c85c40a9583df5387265d642acf@github.com/mooniak/stick-no-bills-font.git"
 git fetch upstream
 git reset upstream/gh-pages
 
