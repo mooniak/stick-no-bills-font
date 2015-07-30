575e326151e778f12fc261f9a00eff3762c0001c
Pathum Egodawatta <pathumego@gmail.com>
2015-07-30 19:38:51 +0530

    Update

---
+++ scripts/_site/update-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
+cd ../sources/
+
+fontforge ../scripts/fontconvert StickNoBills-Bold.sfd --otf
+fontforge ../scripts/fontconvert StickNoBills-Light.sfd --otf
+fontforge ../scripts/fontconvert StickNoBillsTamil-Bold.sfd --otf
+fontforge ../scripts/fontconvert StickNoBillsTamil-Light.sfd --otf
+fontforge ../scripts/fontconvert StickNoBillsSinhala-Bold.sfd --otf
+fontforge ../scripts/fontconvert StickNoBillsSinhala-Light.sfd --otf
+
+
+mv *otf ../tests/fonts
+cd ../tests/
+git add .
+git commit -m 'Updating Tests Doc'
