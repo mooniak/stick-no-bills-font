502b3372470be7cad0092e1dff36979f6e11495e
kosalaya <kombuwaonline@gmail.com>
2015-09-15 13:36:53 +0530

    sh script updated

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
-cd ../sources/
+cd ../sources/sfd/
 
-fontforge ../scripts/fontconvert StickNoBills-Bold.sfd --otf
-fontforge ../scripts/fontconvert StickNoBills-Light.sfd --otf
-fontforge ../scripts/fontconvert StickNoBillsTamil-Bold.sfd --otf
-fontforge ../scripts/fontconvert StickNoBillsTamil-Light.sfd --otf
-fontforge ../scripts/fontconvert StickNoBillsSinhala-Bold.sfd --otf
-fontforge ../scripts/fontconvert StickNoBillsSinhala-Light.sfd --otf
+fontforge ../scripts/fontconvert StickNoBills-latin-1.sfd --otf
+fontforge ../scripts/fontconvert StickNoBills-latin-0.sfd --otf
+fontforge ../scripts/fontconvert StickNoBills-tamil-1.sfd --otf
+fontforge ../scripts/fontconvert StickNoBills-tamil-0.sfd --otf
+fontforge ../scripts/fontconvert StickNoBills-sinhala-1.sfd --otf
+fontforge ../scripts/fontconvert StickNoBills-sinhala-0.sfd --otf
 
 
-mv *otf ../tests/fonts
-cd ../tests/
+mv *otf ../../tests/fonts
+cd ../../tests/
 git add .
 git commit -m 'Updating Tests Doc'
