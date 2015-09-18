87b8bd94b2255f24ee19a0b4acaa6e5bb0325bd1
kosalaya <kombuwaonline@gmail.com>
2015-09-18 22:34:56 +0530

    update

--- scripts/update-tests.sh
+++ scripts/update-tests.sh
@@ -2,12 +2,12 @@
 #Run this for automation: fswatch -0 sources/ | xargs -0 -n1 -I{} sh scripts/update-tests.sh
 cd ../sources/sfd/
 
-fontforge ../scripts/fontconvert StickNoBills-latin-1.sfd --otf
-fontforge ../scripts/fontconvert StickNoBills-latin-0.sfd --otf
-fontforge ../scripts/fontconvert StickNoBills-tamil-1.sfd --otf
-fontforge ../scripts/fontconvert StickNoBills-tamil-0.sfd --otf
-fontforge ../scripts/fontconvert StickNoBills-sinhala-1.sfd --otf
-fontforge ../scripts/fontconvert StickNoBills-sinhala-0.sfd --otf
+fontforge ../../scripts/fontconvert StickNoBills-latin-1.sfd --otf
+fontforge ../../scripts/fontconvert StickNoBills-latin-0.sfd --otf
+fontforge ../../scripts/fontconvert StickNoBills-tamil-1.sfd --otf
+fontforge ../../scripts/fontconvert StickNoBills-tamil-0.sfd --otf
+fontforge ../../scripts/fontconvert StickNoBills-sinhala-1.sfd --otf
+fontforge ../../scripts/fontconvert StickNoBills-sinhala-0.sfd --otf
 
 
 mv *otf ../../tests/fonts
