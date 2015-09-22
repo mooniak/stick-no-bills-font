3617f94180d3e109467ecf7a3f3ab4b82449dc7a
paarandika <paarandika@gmail.com>
2015-09-23 00:57:01 +0530

    Corrected errors in build scripts

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -2,12 +2,12 @@
 
 cd ../sources/sfd
 
-fontforge ../../scripts/fontconvert StickNoBills-sinhala-0.sfd --ufo
-fontforge ../../scripts/fontconvert StickNoBills-sinhala-1.sfd --ufo
-fontforge ../../scripts/fontconvert StickNoBills-latin-0.sfd --ufo
-fontforge ../../scripts/fontconvert StickNoBills-latin-1.sfd --ufo
-fontforge ../../scripts/fontconvert StickNoBills-tamil-0.sfd --ufo
-fontforge ../../scripts/fontconvert StickNoBills-tamil-1.sfd --ufo
+python ../../scripts/fontconvert StickNoBills-sinhala-0.sfd --ufo
+python ../../scripts/fontconvert StickNoBills-sinhala-1.sfd --ufo
+python ../../scripts/fontconvert StickNoBills-latin-1.sfd --ufo
+python ../../scripts/fontconvert StickNoBills-latin-0.sfd --ufo
+python ../../scripts/fontconvert StickNoBills-tamil-0.sfd --ufo
+python ../../scripts/fontconvert StickNoBills-tamil-1.sfd --ufo
 
 mv StickNoBills-sinhala-0.ufo ../../sources
 mv StickNoBills-sinhala-1.ufo ../../sources
