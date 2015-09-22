90b5f193a8d73efd76eed32ec8cfe2da7898c966
paarandika <paarandika@gmail.com>
2015-09-23 01:26:53 +0530

    Added merger.py

---
+++ scripts/merger.py
@@ -0,0 +1,36 @@
+from robofab.world import NewFont, OpenFont
+from robofab import plistlib
+import sys, os
+def ufoGlyphOrderSetter(existingOrder, newOrder):
+    outOrder=[]
+    for glyphName in newOrder:
+        if glyphName not in existingOrder:
+            outOrder.append(glyphName)
+    return outOrder
+
+arguments=sys.argv
+fontList=arguments[2:]
+print fontList
+print "merging fonts!"
+print os.getcwd()
+NewUFO = NewFont()
+font_source = OpenFont(fontList[0])
+for glyphName in ufoGlyphOrderSetter([], font_source.keys()):
+    glyph = font_source[glyphName]
+    NewUFO.insertGlyph(glyph)
+    print glyphName
+    NewUFO[glyphName].unicode = font_source[glyphName].unicode
+NewUFO.lib=font_source.lib
+NewUFO.info=font_source.info
+
+for font in fontList[1:]:
+    source= OpenFont(font)
+    for glyph_name in ufoGlyphOrderSetter(NewUFO.keys(), source.keys()):
+        glyph = source[glyph_name]
+        print glyph_name
+        NewUFO.insertGlyph(glyph)
+        NewUFO[glyphName].unicode = font_source[glyphName].unicode
+    newLib=[i for i in source.lib['public.glyphOrder'] if i not in NewUFO.lib['public.glyphOrder']]+NewUFO.lib['public.glyphOrder']
+    NewUFO.lib['public.glyphOrder']=newLib
+
+NewUFO.save(arguments[1])
