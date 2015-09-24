ed9797f08989c6d94aa12baf79cefc32990aaf6b
paarandika <paarandika@gmail.com>
2015-09-24 18:59:04 +0530

    removed mv to avoid avoid Vagrant file operation issues on Windows

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -2,19 +2,12 @@
 
 cd ../sources/sfd
 
-python ../../scripts/fontconvert StickNoBills-sinhala-0.sfd --ufo
-python ../../scripts/fontconvert StickNoBills-sinhala-1.sfd --ufo
-python ../../scripts/fontconvert StickNoBills-latin-1.sfd --ufo
-python ../../scripts/fontconvert StickNoBills-latin-0.sfd --ufo
-python ../../scripts/fontconvert StickNoBills-tamil-0.sfd --ufo
-python ../../scripts/fontconvert StickNoBills-tamil-1.sfd --ufo
-
-mv StickNoBills-sinhala-0.ufo ../../sources
-mv StickNoBills-sinhala-1.ufo ../../sources
-mv StickNoBills-latin-0.ufo ../../sources
-mv StickNoBills-latin-1.ufo ../../sources
-mv StickNoBills-tamil-0.ufo ../../sources
-mv StickNoBills-tamil-1.ufo ../../sources
+python ../../scripts/fontconvert StickNoBills-sinhala-0.sfd ../../sources --ufo
+python ../../scripts/fontconvert StickNoBills-sinhala-1.sfd ../../sources --ufo
+python ../../scripts/fontconvert StickNoBills-latin-1.sfd ../../sources --ufo
+python ../../scripts/fontconvert StickNoBills-latin-0.sfd ../../sources --ufo
+python ../../scripts/fontconvert StickNoBills-tamil-0.sfd ../../sources --ufo
+python ../../scripts/fontconvert StickNoBills-tamil-1.sfd ../../sources --ufo
 
 cd ../../scripts
 python merger.py ../masters/StickNoBills-Regular.ufo ../sources/StickNoBills-sinhala-0.ufo ../sources/StickNoBills-latin-0.ufo ../sources/StickNoBills-tamil-0.ufo
