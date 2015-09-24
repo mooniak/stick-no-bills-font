785e20f06c0d89724726bd46976773267cae8dd2
paarandika <paarandika@gmail.com>
2015-09-24 20:48:33 +0530

    Glyph names on same line

--- scripts/merger.py
+++ scripts/merger.py
@@ -10,15 +10,15 @@ def ufoGlyphOrderSetter(existingOrder, newOrder):
 
 arguments=sys.argv
 fontList=arguments[2:]
-print fontList
-print "merging fonts!"
+
+print "Merging fonts..."
 print os.getcwd()
 NewUFO = NewFont()
 font_source = OpenFont(fontList[0])
 for glyphName in ufoGlyphOrderSetter([], font_source.keys()):
     glyph = font_source[glyphName]
     NewUFO.insertGlyph(glyph)
-    print glyphName
+    print glyphName,
     NewUFO[glyphName].unicode = font_source[glyphName].unicode
 NewUFO.lib=font_source.lib
 NewUFO.info=font_source.info
@@ -27,10 +27,11 @@ for font in fontList[1:]:
     source= OpenFont(font)
     for glyph_name in ufoGlyphOrderSetter(NewUFO.keys(), source.keys()):
         glyph = source[glyph_name]
-        print glyph_name
+        print glyph_name,
         NewUFO.insertGlyph(glyph)
         NewUFO[glyphName].unicode = font_source[glyphName].unicode
     newLib=[i for i in source.lib['public.glyphOrder'] if i not in NewUFO.lib['public.glyphOrder']]+NewUFO.lib['public.glyphOrder']
     NewUFO.lib['public.glyphOrder']=newLib
 
 NewUFO.save(arguments[1])
+print "\nMerge complete!"
