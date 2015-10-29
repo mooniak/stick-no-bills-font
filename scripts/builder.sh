634df83e88147ef990ebdbaceecdcc6e646c665b
paarandika <paarandika@gmail.com>
2015-10-29 07:21:49 +0530

    Removed Tamil

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -16,12 +16,10 @@ python ../../scripts/fontconvert StickNoBills-sinhala-0.sfd ../../sources --ufo
 python ../../scripts/fontconvert StickNoBills-sinhala-1.sfd ../../sources --ufo
 python ../../scripts/fontconvert StickNoBills-latin-1.sfd ../../sources --ufo
 python ../../scripts/fontconvert StickNoBills-latin-0.sfd ../../sources --ufo
-python ../../scripts/fontconvert StickNoBills-tamil-0.sfd ../../sources --ufo
-python ../../scripts/fontconvert StickNoBills-tamil-1.sfd ../../sources --ufo
 
 cd ../../scripts
-python merger.py ../masters/StickNoBills-Regular.ufo ../sources/StickNoBills-sinhala-0.ufo ../sources/StickNoBills-latin-0.ufo ../sources/StickNoBills-tamil-0.ufo
-python merger.py ../masters/StickNoBills-ExtraBold.ufo ../sources/StickNoBills-sinhala-1.ufo ../sources/StickNoBills-latin-1.ufo ../sources/StickNoBills-tamil-1.ufo
+python merger.py ../masters/StickNoBills-Regular.ufo ../sources/StickNoBills-sinhala-0.ufo ../sources/StickNoBills-latin-0.ufo
+python merger.py ../masters/StickNoBills-ExtraBold.ufo ../sources/StickNoBills-sinhala-1.ufo ../sources/StickNoBills-latin-1.ufo
 
 cd ../
 python build.py
