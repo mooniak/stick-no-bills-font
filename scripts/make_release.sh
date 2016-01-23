1737fb66fc4b260beb0d9fa886975655e5db7695
Pathum Egodawatta <pathumego@gmail.com>
2016-01-23 13:28:51 +0000

    Update make release script

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Run this after building the fonts. It will make a zip including built binaries, OFL and fontlog.
-cd ../;
-FOLDER="/build"
+cd ../
+FOLDER="./build"
 FAMILY="${PWD##*/}"
 DATE=$(date +%Y%m%d)
 echo -n "Enter just the version number[ENTER]: "
@@ -10,7 +10,7 @@ echo
 cp OFL.txt $FOLDER
 cp FONTLOG.md $FOLDER
 mv $FOLDER/FONTLOG.md $FOLDER/FONTLOG.txt
-zip -r $FAMILY'_v'$version'_'$DATE.zip /build
-rm $FOLDER/OFL.txt 
-rm $FOLDER/FONTLOG.txt
+mv build $FAMILY'_v'$version'_'$DATE
+zip -r $FAMILY'_v'$version'_'$DATE.zip $FAMILY'_v'$version'_'$DATE/.
+mv $FAMILY'_v'$version'_'$DATE build
 echo DONE!
